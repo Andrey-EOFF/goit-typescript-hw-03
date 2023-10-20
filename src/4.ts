8a1fecd9a0765d0c38faefdf3764d1f313cf7e5d
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-10-20 12:25:32 +0300

    update-task4

--- src/4.ts
+++ src/4.ts
@@ -1,26 +1,4 @@
-// Завдання 4 *
-// У цьому завдання вам належить реалізувати сценарій життя, де людина, ключ і будинок взаємодіють один з одним.
 
-//++++++++++++++++++++++ Ключ (Key): Створіть клас Key. У нього має бути одна приватна властивість signature, яка генерується випадково при створенні об'єкта цього класу (наприклад Math.random()). Також цей клас повинен мати метод getSignature, який повертає значення властивості signature.
-
-// Людина (Person): Створіть клас Person. Конструктор цього класу приймає об'єкт класу Key і зберігає їх у приватному властивості key. Клас Person повинен мати метод getKey, який повертає збережений ключ.
-
-// Дім (House): Створіть абстрактний клас House. Цей клас має дві властивості: door, яка може бути відкрита (true), або закрита (false), і key, яка зберігає об'єкт класу Key. У цьому класі також повинен бути метод comeIn, який додає об'єкт класу Person у масив tenants, якщо door відкрита. Ваш абстрактний клас House також повинен мати абстрактний метод OpenDoor, який приймає об'єкт класу Key.
-
-// Мій будинок (MyHouse): Створіть клас MyHouse, який успадковується від абстрактного класу House. Реалізуйте метод openDoor у цьому класі. Якщо ключ, переданий цьому методу, збігається з ключем, збереженим як key, то двері відчиняються.
-
-// Після реалізації всіх класів створіть об'єкти для кожного класу та спробуйте відтворити сценарій, в якому людина приходить додому.
-
-// Наприклад, ось так:
-
-// const key = new Key();
-
-// const house = new MyHouse(key);
-// const person = new Person(key);
-
-// house.openDoor(person.getKey());
-
-// house.comeIn(person);
 class Key {
     private signature: number;
 
@@ -46,7 +24,7 @@ class Person {
 }
 
 abstract class House {
-    protected door: boolean;
+    protected door: boolean = false;
     protected key: Key;
     protected tenants: Person[] = [];
 
@@ -54,7 +32,7 @@ abstract class House {
         this.key = key;
     }
 
-    comeIn() {
+    comeIn(person: Person):void {
         if (this.door) {
             this.tenants.push(person)
             
@@ -62,13 +40,21 @@ abstract class House {
 
     }
 
-    abstract OpenDoor(key: Key): void;
+    abstract openDoor(key: Key): void;
 }
 
 class MyHouse extends House {
-    OpenDoor(key: Key): void {
+
+    constructor(key: Key) {
+        super(key);
+    }
+
+    openDoor(key: Key): void {
         if (key.getSignature() === this.key.getSignature()) {
-            
+            this.door = true;
+        }
+        else {
+            this.door = false;
         }
     }
 }
